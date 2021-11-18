780ce421a790f80776d03b96f19374166d019821
jch <jchenschel@gmail.com>
2021-11-18 08:38:49 -0600

    test

---
+++ 07_chapter/01_rental_car.py
@@ -0,0 +1,2 @@
+# Write a program that asks the user what kind of rental car they would like.
+# Print a message about that car, such as “Let me see if I can find you a Subaru.”
---
+++ 07_chapter/02_restaurant_seating.py
@@ -0,0 +1,3 @@
+# Write a program that asks the user how many people are in their
+# dinner group. If the answer is more than eight, print a message saying they’ll have to wait for a
+# table. Otherwise, report that their table is ready.
